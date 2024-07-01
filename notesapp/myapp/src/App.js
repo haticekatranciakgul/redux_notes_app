066895abef535b0f040fdadf1b3dfa492462d4a6
Hatice KATRANCI AKGÜL <76768140+haticekatranciakgul@users.noreply.github.com>
2024-07-01 19:06:46 +0300

    created project

---
+++ notesapp/myapp/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        created project
+      </header>
+    </div>
+  );
+}
+
+export default App;
